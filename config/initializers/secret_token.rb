959e19d7be075e654b01c993cb2e7a69d60088e6
Felix Li <leegyao@gmail.com>
2013-02-28 10:33:08 +0800

    Initialize env and travis ci.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsEshop::Application.config.secret_token = 'de7b41345cceb64392a320f2cf491065e4adeaddeba018b1272923d37924508b3eadb51e03c9653cec6efd741b3dc5229167966e1ca646c81127a4aaa47136f8'
