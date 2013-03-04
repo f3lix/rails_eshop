36165d243fc7013e9cc35e62afd9713989cdfd26
Felix Li <leegyao@gmail.com>
2013-03-05 01:13:19 +0800

    Add new controller 'Store'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsEshop::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
 
@@ -51,7 +53,7 @@ RailsEshop::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
