ae73a1887382ab8ff99c25dce0cf9df8c9143592
Felix Li <leegyao@gmail.com>
2013-03-14 00:46:05 +0800

    Find current shopping cart from session.

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,13 @@
 module CartsHelper
+  def current_cart=(cart)
+  	@current_cart = cart
+  end
+
+  def current_cart?(cart)
+  	cart == current_cart
+  end
+
+  def current_cart
+  	@current_cart ||= Cart.find(session[:cart_id])
+  end
 end
