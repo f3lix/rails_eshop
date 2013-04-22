b38d59af53d67875966851d3335b77138bec8d8b
Felix Li <leegyao@gmail.com>
2013-04-23 07:36:43 +0800

    Add time counting to groupon feature.

--- app/models/groupon.rb
+++ app/models/groupon.rb
@@ -1,3 +1,3 @@
 class Groupon < ActiveRecord::Base
-  attr_accessible :description, :original_price, :price, :title, :stock
+  attr_accessible :description, :original_price, :price, :title, :stock, :created_at
 end
